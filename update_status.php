8215c95f7367ed835e13cf7157737b820781b6da
kunlex23 <kunlex23@gmail.com>
2023-12-20 15:38:36 +0100

    f

--- update_status.php
+++ update_status.php
@@ -4,7 +4,6 @@ if (isset($_GET['fullname'])) {
     // Get the fullname from the URL parameter
     $fullname = urldecode($_GET['fullname']);
 
-    <?php 
     require 'config.php'; 
     // Update the status in the database
     $newStatus = "Done"; // The new status you want to set
